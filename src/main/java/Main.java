d886376cecadeba39f1c385f0b2554759a58c8cf
ddudalves <169068338+ddudalves@users.noreply.github.com>
2024-09-02 12:13:34 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    String nome = "lais";
+    int dia = 9;
+    int mes = 4;
+    int ano = 2009;
+   double pi = 3.14159;
+ 
+    System.out.printf("o valor de pi é: %.4f ",pi );
+
+
+
+}
